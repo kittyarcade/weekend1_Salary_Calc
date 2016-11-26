5fd1cf475703bd42c65780feb0c147897bf4195f
Amanda Jopp <aclaire488@gmail.com>
2016-11-26 11:44:18 -0600

    js added

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 console.log('js sourced');
 
-var employee = ["first name", "lastName", "idNum", "jobTitle", "annSal"];
+var employees= [];
+
+
+
+var newEmp = function(firstName, lastName, idNum, jobTitle, annSal){
+
+  var emp = {
+    firstName: firstName,
+    lastName: lastName,
+    idNum: idNum,
+    jobTitle: jobTitle,
+    annSal: annSal
+  };
+  employees.push(newEmp);
+};
+
+var getEmp = function(){
+
+var fName = document.getElementById('firstName').value;
+var lName = document.getElementById('lastName').value;
+var id = document.getElementById('idNum').value;
+var title = document.getElementById('jobTitle').value;
+var salary = document.getElementById('annSal').value;
+
+};
